ab60bd29297b8c5cd74c0485f105123824a74155
mateusz <mateschm@gmail.com>
2024-02-23 13:31:32 +0000

    Let user change the size of the grid

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const container = document.querySelector('.container');
+sideLength = 16;
 
-for  (let i = 0; i < 256; i++) {
+for  (let i = 0; i < sideLength**2; i++) {
     const cell = document.createElement('div');
     cell.classList.add(`cell`);
     cell.style.cssText = `border: solid white; flex-grow: 0; 
-    flex-basis: 6.25%; box-sizing: border-box; border-width: 1px`;
+    flex-basis: ${100/sideLength}%; box-sizing: border-box; border-width: 1px`;
     container.appendChild(cell);
 }
 
-const cells = document.querySelectorAll(`.cell`);
 
-console.log(cells);
+
+const cells = document.querySelectorAll(`.cell`);
 
 cells.forEach((div) => {
     div.addEventListener(`mouseover`, function (e) {
         e.target.style.background = 'white';
     });
 });
+
+let button = document.querySelector(`button`);
+
+button.addEventListener(`click`, createGrid);
+
+function removeGrid () {
+    container.textContent = '';
+}
+
+function createGrid () {
+    sideLength = prompt(`Enter number of squares per side`);
+    removeGrid();
+    if ((sideLength > 100) || (sideLength < 0)) {
+        sideLength = prompt(`Enter value between 2 and 100`);
+    } else {
+        for  (let i = 0; i < sideLength**2; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add(`cell`);
+            cell.style.cssText = `border: solid white; flex-grow: 0; 
+            flex-basis: ${100/sideLength}%; box-sizing: border-box; border-width: 1px`;
+            container.appendChild(cell);
+        }
+    }
+    const cells = document.querySelectorAll(`.cell`);
+    cells.forEach((div) => {
+        div.addEventListener(`mouseover`, function (e) {
+            e.target.style.background = 'white';
+        });
+    });
+}
